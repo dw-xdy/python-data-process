851812feb06ab3cfec021f563e3f621417447a24
dw-xdy <3115708251@qq.com>
2025-12-24 00:13:19 +0800

    经过验证, 没有问题, 但是还是需要后续的优化

--- src/data-process/school_exam_data_process.py
+++ src/data-process/school_exam_data_process.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from typing import Union
 
 import polars as pl
-from jinja2.nodes import Output
 
 
 def excel_data_process(
@@ -47,6 +46,18 @@ def batch_excel_data_process(
         keep_column: list[str],
         output_folder: str = None,
 ) -> None:
+    """
+    关于这个方法:  是用来批量整理学校发的excel (必须是.xlsx后缀) 表中的数据, 并进行清洗,
+    是一个非常简单的任务处理, 并不是非常专业的处理方式, 但是我想用来处理学校的excel绝对是足够了.
+    可以根据需求决定是否需要转换成: word, 或者进一步转换成: PDF.
+    后面会有对应的提示.
+    :param input_folder: 必须是一个文件夹的路径
+    :param judge_column: 根据这一列来判断行中的元素是不是需要保留.
+    :param keep_row: 存放你想要保留的行
+    :param keep_column: 需要保留的列.
+    :param output_folder: 必须是保存文件夹子的路径
+    :return: None
+    """
     # 转换为 Path 对象
     input_path = Path(input_folder)
 
@@ -81,9 +92,22 @@ def batch_excel_data_process(
 
         print(f"{excel_file.name}处理完成")
 
+    print("-" * 50)
     # 显示输出文件夹内容
-    excel_files_finish = list(output_folder.glob("*.xlsx"))
+    excel_files_finish = list(output_path.glob("*.xlsx"))
     if excel_files_finish:
         print(f"生成Excel文件数量: {len(excel_files_finish)} 个")
     else:
         print("警告: 输出文件夹中没有找到Excel文件")
+
+
+input_folder = r"C:\Users\asus\Desktop\学校作业\信息论"
+output_folder = None
+
+judge_column = "题型"
+
+keep_row = ["单选题"]
+
+keep_column = ["题干", "正确答案", "选项A", "选项B", "选项C", "选项D"]
+
+batch_excel_data_process(input_folder, judge_column, keep_row, keep_column, output_folder)
